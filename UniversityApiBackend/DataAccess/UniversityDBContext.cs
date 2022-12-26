a2bf7e653d02adbbeedfdc19e70949f21ff4ffd3
feder <feder@FEDERICO>
2022-12-26 17:11:06 -0300

    Agregar archivos de proyecto.

---
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options) : base(options) 
+        {
+        }
+
+        //TODO Tablas de la base de datos
+        public DbSet<User>? Users { get; set; }
+    }
+}
---
+++ UniversityApiBackend/Models/DataModels/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class User : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Nombre { get;set; } = string.Empty;
+        [Required, StringLength(100)]
+        public string Apellido { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
